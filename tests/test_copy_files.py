8d2ae362b78bef74167950bece46a7bfb26613fa
Ridvan Gider <ridvan.gider@adesso.com.tr>
2026-03-17 12:50:48 +0300

    added recursive copy test case

--- tests/test_copy_files.py
+++ tests/test_copy_files.py
@@ -9,3 +9,16 @@ def test_copy_file(spark, workspace_client):
     result = spark.sql("LIST '/Volumes/ghcn_dev/bronze/raw/'").collect()
     file_names = [row.path for row in result]
     assert any("readme.txt" in path for path in file_names)
+
+
+def test_copy_file_recursive(spark, workspace_client):
+    from ghcn.copy_files import copy_files
+
+    source = "s3://noaa-ghcn-pds/parquet/by_year/YEAR=1750/"
+    destination = "/Volumes/ghcn_dev/bronze/raw/parquet/by_year/YEAR=1750/"
+
+    copy_files(workspace_client, source, destination, recursive=True)
+
+    result = spark.sql("LIST '/Volumes/ghcn_dev/bronze/raw/parquet/by_year/YEAR=1750/'").collect()
+    file_names = [row.path for row in result]
+    assert len(file_names) > 0
